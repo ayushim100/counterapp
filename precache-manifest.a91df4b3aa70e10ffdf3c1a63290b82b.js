1dc7cefff79b571c4b0dc3580756210fa91f5c12
Ayushi Maheshwari <ayushim100@gmail.com>
2021-11-24 11:07:45 +0530

    Updates

---
+++ precache-manifest.a91df4b3aa70e10ffdf3c1a63290b82b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7044d422cbbc2c3a06af85c0fc1cd4",
+    "url": "/counterapp/index.html"
+  },
+  {
+    "revision": "7cafba4fd66bd9f150f0",
+    "url": "/counterapp/static/css/main.a0f6c0c5.chunk.css"
+  },
+  {
+    "revision": "8384f15cef5cb2632a4f",
+    "url": "/counterapp/static/js/2.f6f1eb51.chunk.js"
+  },
+  {
+    "revision": "ea88211925d18270e09b4691b32a2900",
+    "url": "/counterapp/static/js/2.f6f1eb51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cafba4fd66bd9f150f0",
+    "url": "/counterapp/static/js/main.000a9655.chunk.js"
+  },
+  {
+    "revision": "416aa84507bfe22ab290",
+    "url": "/counterapp/static/js/runtime-main.2d3c8383.js"
+  },
+  {
+    "revision": "b1abb878e2529cb5cb4450139844155d",
+    "url": "/counterapp/static/media/AvenirLTStd-Black.b1abb878.otf"
+  },
+  {
+    "revision": "b1d7c6e085a31e9f5e4745c9aef6eb4b",
+    "url": "/counterapp/static/media/AvenirLTStd-Roman.b1d7c6e0.otf"
+  }
+]);
